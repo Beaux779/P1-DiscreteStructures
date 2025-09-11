383c2c979722da8a2af7e2207f003df2352f42ee
Beaux779 <beauxrdolf@icloud.com>
2025-09-11 18:07:57 -0500

    Removed functions for simplicity, hardcoding next

--- P1.cpp
+++ P1.cpp
@@ -3,15 +3,8 @@
 
 using namespace std;
 
-bool imp(bool a,bool b);   // a implies b
-bool iff(bool a,bool b);   // a if and only if b
-bool xor2(bool a,bool b);  // a exclusive or b
-void printHeader();
-void printRow(bool p, bool q, bool r);
-void printSummary();
-
 int main(){
-    bool p, q, r;
+    int p, q;
 
 
 
@@ -20,14 +13,4 @@ int main(){
 
 
     return 0;
-}
-
-bool imp(bool a, bool b){ // a implies b
-    return (a == true && b == false) ? false : true;
-}
-bool iff(bool a, bool b){ // a if and only if b
-    return (a == b)? true : false;
-}
-bool xor2(bool a, bool b){ // a exclusive or b
-    return (a != b)? true : false;
 }
