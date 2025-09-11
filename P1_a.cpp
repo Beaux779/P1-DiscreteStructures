a1f5871a21a2a156339c22a683fda335e9d3e479
Jacob Simon <jacob41simon@gmail.com>
2025-09-11 18:53:26 -0500

    Initial commit: Added 2st P1 iteration: added expression 3

--- P1_a.cpp
+++ P1_a.cpp
@@ -16,19 +16,44 @@ int main(){
     cout << "Check truth value for: Expression 1" << endl;
 
     int trueCount = 0, totalCases = 0;
-    for (int p = 0; p <= 1; p++) {
-        for (int q = 0; q <= 1; q++) {
+    for(int p = 0; p <= 1; p++) {
+        for(int q = 0; q <= 1; q++) {
             bool expr1 = ((p || q) && (!p && !q));
             cout << p << " " << q << " " << expr1 << endl;
-            if (expr1) trueCount++;
+            if(expr1) trueCount++;
             totalCases++;
         }
     }
 
     cout << "The amount of true cases are " << trueCount << endl;
-    if (trueCount == 0)
+    if(trueCount == 0)
         cout << "Expression 1 is a contradiction." << endl;
-    else if (trueCount == totalCases)
+    else if(trueCount == totalCases)
+        cout << "Expression 1 is a tautology." << endl;
+    else
+        cout << "Expression 1 is a contingency." << endl;
+    cout << endl;
+
+
+
+    cout << "Check truth value for: Expression 3" << endl;
+
+    int trueCount = 0, totalCases = 0;
+    for(int p = 0; p <= 1; p++) {
+        for(int q = 0; q <= 1; q++) {
+            for(int r =0; r <= 1; r++) {
+                bool expr3 = imp(((p||q) && (!p||r)), (p && r));
+                cout << p << " " << q << " " << r << " " << expr3 << endl;
+                if (expr3) trueCount++;
+                totalCases++;
+            }
+        }
+    }
+
+    cout << "The amount of true cases are " << trueCount << endl;
+    if(trueCount == 0)
+        cout << "Expression 1 is a contradiction." << endl;
+    else if(trueCount == totalCases)
         cout << "Expression 1 is a tautology." << endl;
     else
         cout << "Expression 1 is a contingency." << endl;
