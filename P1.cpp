51118cb7cc86b46afd743b6704259b7fc4a9afe9
Beaux779 <beauxrdolf@icloud.com>
2025-09-11 18:36:11 -0500

    oops

--- P1.cpp
+++ P1.cpp
@@ -3,8 +3,15 @@
 
 using namespace std;
 
+bool imp(bool a,bool b);   // a implies b
+bool iff(bool a,bool b);   // a if and only if b
+bool xor2(bool a,bool b);  // a exclusive or b
+void printHeader();
+void printRow(bool p, bool q, bool r);
+void printSummary();
+
 int main(){
-    int p, q;
+    bool p, q, r;
 
 
 
@@ -13,4 +20,14 @@ int main(){
 
 
     return 0;
+}
+
+bool imp(bool a, bool b){ // a implies b
+    return (a == true && b == false) ? false : true;
+}
+bool iff(bool a, bool b){ // a if and only if b
+    return (a == b)? true : false;
+}
+bool xor2(bool a, bool b){ // a exclusive or b
+    return (a != b)? true : false;
 }
