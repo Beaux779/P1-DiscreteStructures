98dc56b412ae0281a41d1a2804d97b5cbefe7851
Beaux779 <beauxrdolf@icloud.com>
2025-09-11 19:17:00 -0500

    expression 2 done, some edits on other expressions

--- P1_a.cpp
+++ P1_a.cpp
@@ -37,39 +37,53 @@ int main(){
 
 
     //expression 2
+    // (p <-> q) -> (p <-> q)  (tautology)
     cout << "Check truth value for: Expression 2" << endl;
+    cout << "p q p<->q expr2\n";
+    int trueCount2 = 0, totalCases2 = 0;
+    for (int p = 0; p <= 1; p++) {
+        for (int q = 0; q <= 1; q++) {
+            int expr2 = imp(iff(p, q), iff(p, q));
+            cout << p << " " << q << " " << iff(p, q) << " " << expr2 << endl;
 
+            if (expr2) trueCount2++;
+            totalCases2++; 
+        }
+    }
+    cout << "The amount of true cases are " << trueCount2 << endl;
+    if(trueCount2 == 0)
+        cout << "Expression 2 is a contradiction." << endl;
+    else if(trueCount2 == totalCases2)
+        cout << "Expression 2 is a tautology." << endl;
+    else
+        cout << "Expression 2 is a contingency." << endl;
 
 
     //expression 3
     cout << "Check truth value for: Expression 3" << endl;
 
-    int trueCount2 = 0, totalCases2 = 0;
+    int trueCount3 = 0, totalCases3 = 0;
     for(int p = 0; p <= 1; p++) {
         for(int q = 0; q <= 1; q++) {
             for(int r =0; r <= 1; r++) {
                 bool expr3 = imp(((p||q) && (!p||r)), (p && r));
                 cout << p << " " << q << " " << r << " " << expr3 << endl;
-                if (expr3) trueCount2++;
-                totalCases2++;
+                if (expr3) trueCount3++;
+                totalCases3++;
             }
         }
     }
 
 
-    //expression 4
-
-
-
-    cout << "The amount of true cases are " << trueCount2 << endl;
-    if(trueCount2 == 0)
-        cout << "Expression 1 is a contradiction." << endl;
-    else if(trueCount2 == totalCases2)
-        cout << "Expression 1 is a tautology." << endl;
+    cout << "The amount of true cases are " << trueCount3 << endl;
+    if(trueCount3 == 0)
+        cout << "Expression 3 is a contradiction." << endl;
+    else if(trueCount3 == totalCases3)
+        cout << "Expression 3 is a tautology." << endl;
     else
-        cout << "Expression 1 is a contingency." << endl;
+        cout << "Expression 3 is a contingency." << endl;
     cout << endl;
-
+    //expression 4
     
 
 
